5216836b98ccbe5e18264e58d8ed1912c8f20711
Corniel Nobel <corniel@gmail.com>
2025-04-23 11:58:27 +0200

    Use Qowaiv v7.3.0-rc.1

--- src/Qowaiv.Identifiers/GuidBehavior.cs
+++
@@ -1,6 +0,0 @@
-namespace Qowaiv.Identifiers;
-
-public abstract class GuidBehavior : IdBehavior<Guid>
-{
-    public virtual Guid Next() => Guid.NewGuid();
-}
--- src/Qowaiv.Identifiers/IdBehavior.cs
+++
@@ -1,7 +0,0 @@
-namespace Qowaiv.Identifiers;
-
-public interface IdBehavior<TId> where TId : IEquatable<TId>
-{
-    TId Next();
-}
-
